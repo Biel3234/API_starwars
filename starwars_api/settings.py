87ce013a10cc4d20a240be42ece7f16ef1f341cc
Antonio Gabriel <biepe07@gmail.com>
2026-02-02 21:41:07 -0300

    Criando o app dos personages e instalando

--- starwars_api/settings.py
+++ starwars_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'personagens'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
