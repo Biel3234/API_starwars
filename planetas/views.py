b08c8dce7f66c57ee9bfa37a82ff15f6c7c52b74
Antonio Gabriel <biepe07@gmail.com>
2026-02-04 19:17:23 -0300

    Criado o endpoint dos planetas

--- planetas/views.py
+++ planetas/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from .serializers import PlanetaSerializer
+from .services import fetch_planetas
+
+class PlanetasListView(APIView):
+    def get(self, request):
+        planetas = fetch_planetas() # Todos os planetas
+
+        planetas_filtrados = planetas
+
+        # Filtros
+        name = request.query_params.get("name") # Pega o nome do planeta na URL
+        climate = request.query_params.get("climate") # Pega o clima do planeta na URL
+        terrain = request.query_params.get("terrain") # Pega o terreno do planeta na URL
+        population = request.query_params.get("population") # Pega a população do planeta na URL
+        gravity = request.query_params.get("gravity") # Pega a gravidade do planeta na URL
+
+        if name:
+            planetas_filtrados = [
+                p for p in planetas_filtrados if name.lower().replace(" ", "") in p['name'].lower().replace(" ", "")
+            ]
+        
+        if climate:
+            planetas_filtrados = [
+                p for p in planetas_filtrados if climate.lower().replace(" ", "") in p['climate'].lower().replace(" ", "")
+            ]
+
+        if terrain:
+            planetas_filtrados = [
+                p for p in planetas_filtrados if terrain.lower().replace(" ", "") in p['terrain'].lower().replace(" ", "")
+            ]
+
+        if population:
+            planetas_filtrados = [
+                p for p in planetas_filtrados if population.lower() == p['population'].lower()
+            ]
+
+        if gravity:
+            planetas_filtrados = [
+                p for p in planetas_filtrados if gravity.lower().replace(" ", "") in p['gravity'].lower().replace(" ", "")
+            ]
+
+        
+        # Tratando erro caso nao encontre o planeta
+        if not planetas_filtrados:
+            return Response(
+                {"detail": "Nenhum planeta encontrado"},
+                status=status.HTTP_200_OK
+            )   
+        
+        serializer = PlanetaSerializer(planetas_filtrados, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- starwars_api/urls.py
+++ starwars_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("personagens.urls")),
     path('api/', include("naves.urls")),
+    path('api/', include("planetas.urls")),
 ]
